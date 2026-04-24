d0dbfe56e00742245afe0fb62cd1c6c9bc0939db
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-24 18:08:35 +0000

    Added : Variables & Arithmetic Operators

---
+++ 01-basics/arithmetic.js
@@ -0,0 +1,74 @@
+console.log("--------------------------------------------------")
+console.log("Arithmetic Operators");
+
+let students = 30;
+
+sum = 30 + 2;
+difference = 30 - 2;
+product = 30 * 2;
+remainder = 30 / 2;
+
+console.log(`sum : ${sum} | difference : ${difference} | product : ${product} | remainder : ${remainder}`);
+
+students = 30;
+//add and assign
+students += 2;
+console.log(`Add and Assign : ${students}`);
+
+//minus and assign
+students = 30;
+students -= 2;
+console.log(`Minus and Assign : ${students}`);
+
+//product and assign
+students = 30;
+students *= 2;
+console.log(`Multiply and Assign : ${students}`);
+
+//divide and assign
+students = 30;
+students /= 2;
+console.log(`Divide and Assign : ${students}`);
+
+//exponent and assign
+students = 30;
+students **= 2;
+console.log(`exponent and Assign : ${students}`);
+
+
+//modulus and assign
+students = 30;
+students /= 2;
+console.log(`modulus and Assign : ${students}`);
+
+//modulus and assign
+students = 30;
+students /= 2;
+console.log(`modulus and Assign : ${students}`);
+
+//Increment Operator
+students = 30;
+students++;
+console.log(`Increment and Assign : ${students}`);
+
+
+//Decrement Operator
+students = 30;
+students--;
+console.log(`Decrement and Assign : ${students}`);
+
+/*
+Operator precedence:
+
+1.parenthesis()
+2.exponents
+3. multiplication & division $ modulus
+4. addition and subraction 
+*/
+
+console.log("Complex Mathematical Problem")
+let result = 1 + 2 * 3 + 4 ** 2;
+let result2 = 12 % 5 + 8 / 2;
+
+console.log(result);
+console.log(result2);
---
+++ 01-basics/variables.js
@@ -0,0 +1,31 @@
+console.log("--------------------------------------------------")
+console.log("Variables");
+
+let age = 24;
+let gpa = 2.1;
+let price = 12.59;
+
+console.log(age);
+
+console.log(gpa);
+
+console.log(`The price is ${price}`);
+
+let firstname = "Jamal";
+
+let fav_food = "Pizza";
+
+let email = "jamal@example.com";
+
+let online = false;
+
+console.log(`Email is ${email}`);
+
+console.log(`Bro is online: ${online}`)
+
+let fullname = "Mohamed Jamal";
+let student = false;
+
+document.getElementById("p1").textContent = fullname;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
