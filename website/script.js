d0391496158779de4aeaf55e235fd929fc043f8e
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-24 14:24:57 +0000

    JavaScript tutorial for beginners 🌐

---
+++ website/script.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+console.log("Pizza")
+
+//window.alert(`This is an alert`);
+//window.alert(`I like pizza`);
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza";
