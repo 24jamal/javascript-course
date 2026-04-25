b5da93c93fa2f21f22f605c701bb09080812b1a8
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 08:41:22 +0000

    Added : Ternary operator ❓ and Switches 💡

---
+++ 02-control-flow/switches.js
@@ -0,0 +1,29 @@
+console.log("--------------------------------------------------")
+console.log("Switches :");
+
+let marks = 10;
+
+switch (true) {
+
+    case (marks > 90 && marks < 100):
+        console.log("O grade");
+        break;
+
+    case (marks > 80 && marks <= 90):
+        console.log("A grade");
+        break;
+    case (marks > 70 && marks <= 80):
+        console.log("B grade");
+        break;
+    case (marks > 60 && marks <= 70):
+        console.log("C grade");
+        break;
+    case (marks > 49 && marks <= 60):
+        console.log("D grade");
+        break;
+    case (marks < 50):
+        console.log("Fail");
+        break;
+    default:
+        console.log("Enter valid marks ");
+}
---
+++ 02-control-flow/ternary-operator.js
@@ -0,0 +1,13 @@
+console.log("--------------------------------------------------")
+console.log("Ternary Operator :");
+
+let age = 98;
+isAdult = age <= 17 ? "youre a child" : "Youre an adult";
+console.log(`Is Adult  : ${isAdult}`)
+
+
+let purchaseAmount = 150;
+let isDiscountApplied;
+isDiscountApplied = purchaseAmount >= 150 ? true : false;
+
+console.log(`Is Discount applied : ${isDiscountApplied}`)
