cdfd4872c8e66fe4f8ee647bf1a8618a0f7dcfc5
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 13:43:30 +0000

    Added : Logical operators ❗ amd Strict equality 🟰

---
+++ 02-control-flow/logical_operators.js
@@ -0,0 +1,21 @@
+console.log("--------------------------------------------------")
+console.log("Logical Operators :");
+
+let temp = 25;
+
+if (temp > 0 && temp < 40) {
+    console.log("Weather is Good");
+}
+else {
+    console.log("Weather is Bad");
+}
+
+let issunny = true;
+
+if (issunny) {
+    console.log("Sunny");
+}
+else {
+    console.log("Not Sunny");
+
+}
---
+++ 02-control-flow/strict_equality.js
@@ -0,0 +1,11 @@
+console.log("--------------------------------------------------")
+console.log("Strict Equality Operators :");
+
+const PI = 3.14;
+
+if (PI === "3.14") {
+    console.log("Equals");
+}
+else {
+    console.log("Not equals");
+}
