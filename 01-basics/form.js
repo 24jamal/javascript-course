f07eec8d21cda857202f8cac9272c900867e38c0
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 04:45:57 +0000

    Added JS Title

--- 01-basics/form.js
+++ 01-basics/form.js
@@ -1,3 +1,6 @@
+console.log("--------------------------------------------------")
+console.log("User Input Acceptance");
+
 document.getElementById("submit").onclick = function () {
     username = document.getElementById("username").value;
     document.getElementById("myh2").textContent = `Hello ${username}`;
