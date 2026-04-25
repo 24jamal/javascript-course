c56e5c4c5eb219f459e763bf936f956d946f74f1
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 16:31:16 +0000

    Added : Arrays 🗃

---
+++ 05-arrays/arrays.js
@@ -0,0 +1,22 @@
+let fruits = ["banana", "coconut", "apple"];
+
+fruits.push("Mango");
+fruits.pop();
+fruits.unshift("Papaya");
+fruits.shift();
+
+let nooffruits = fruits.length;
+let index = fruits.indexOf("apple");
+
+console.log(fruits);
+console.log(nooffruits);
+console.log(index);
+
+for (let i = 0; i < nooffruits; i++) {
+    console.log(fruits[i]);
+}
+
+fruits.sort().reverse();
+for (let fruit of fruits) {
+    console.log(fruit);
+}
