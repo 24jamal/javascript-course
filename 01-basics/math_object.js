2a3c8e4333cde4ba50fba2a17d263ddf07028258
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 04:48:37 +0000

    Added Math Object

---
+++ 01-basics/math_object.js
@@ -0,0 +1,34 @@
+console.log("--------------------------------------------------");
+console.log("Math Object");
+
+console.log(Math.PI);
+let a1 = 3.21;
+let b1 = 2.33;
+
+let rounded = Math.round(a1);
+let floored = Math.floor(a1);
+let ceiled = Math.ceil(a1);
+let truncated = Math.trunc(a1);
+let powered = Math.pow(a1, b1);
+let sqrt = Math.sqrt(a1);
+let log = Math.log(a1);
+let sine = Math.sin(a1);
+let cosine = Math.cos(a1);
+let tan = Math.tan(a1);
+let abs = Math.abs(a1);
+let sign = Math.sign(a1, b1);
+let max = Math.max(a1, b1);
+let min = Math.min(a1, b1);
+
+console.log(`Round : ${rounded}`);
+console.log(`Floor : ${floored}`);
+console.log(`Ceil : ${ceiled}`);
+console.log(`Power : ${powered}`);
+console.log(`Log : ${log}`);
+console.log(`Sine ${sine}`);
+console.log(`Cosine : ${cosine}`);
+console.log(`Tan : ${tan}`);
+console.log(`Absolute : ${abs}`);
+console.log(`Sign : ${sign}`);
+console.log(`Max : ${max}`)
+console.log(`Min : ${min}`)
