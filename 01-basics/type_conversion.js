1a65d75adfaf80637395a5a77a68f48043a1b302
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 04:47:03 +0000

    Added : Type Conversion

---
+++ 01-basics/type_conversion.js
@@ -0,0 +1,28 @@
+console.log("--------------------------------------------------")
+console.log("Type Conversion :");
+
+
+let x = "0";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(x);
+z = Boolean(z);
+
+
+console.log(x, typeof (x));
+console.log(y, typeof (y));
+console.log(z, typeof (z));
+
+let a;
+let b;
+let c;
+
+a = Number(a);
+b = String(b);
+c = Boolean(c);
+
+console.log(a, typeof (a));
+console.log(b, typeof (b));
+console.log(c, typeof (c));
