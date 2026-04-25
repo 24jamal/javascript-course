3c91cc0d9da62318a2a23e97e6188f3b94da3fce
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-25 06:21:42 +0000

    Added : Random and Roll Dice HTML

---
+++ 01-basics/random.js
@@ -0,0 +1,8 @@
+console.log("--------------------------------------------------")
+console.log("Random : ");
+
+let min2 = 50;
+let max2 = 100;
+let random = Math.floor(Math.random() * (max2 - min2)) + min2;
+
+console.log(random);
