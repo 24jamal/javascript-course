9b7ef94d51e008cd5ce7c7bed86d41752d0c2e07
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-26 12:28:28 +0000

    Added: Arrow functions 🎯

---
+++ 04-functions/arrow_functions.js
@@ -0,0 +1,39 @@
+console.log("--------------------------------------------------")
+console.log("Arrow Function :");
+
+//Normal Function Declaration
+
+function hellos() {
+    console.log("Hello");
+}
+
+hellos();
+
+
+//Function Expression
+const hello2 = function () {
+    console.log("Hello");
+}
+
+hello2();
+
+//Arrow Function
+const hello3 = () => console.log("Hello Earth");
+
+const sumnums = (a, b) => a + b;
+
+console.log(`Arrow Sum: ${sumnums(2, 4)}`);
+
+const newnums = [1, 2, 3, 4, 5, 6];
+
+const newsquares = newnums.map((element) => element ** 2);
+
+const newodds = newnums.filter((element) => (element % 2 != 0));
+
+const newevens = newnums.filter((element) => (element % 2 == 0));
+
+const total2 = newnums.reduce((accumulator, element) => accumulator + element)
+
+console.log(`newsquares ${newsquares}`);
+console.log(`newodds ${newodds}`);
+console.log(`total : ${total2}`);
